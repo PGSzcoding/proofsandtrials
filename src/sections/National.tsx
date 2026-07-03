b707f25032768365c14454f348bd75aced09f326
Paola Gutierrez <paola@justsomecoding.com>
2026-07-03 12:33:55 -0600

    National mobile v

--- src/sections/National.tsx
+++ src/sections/National.tsx
@@ -1,23 +1,30 @@
 import Container from "../components/Container";
 import colombiaMap from "../assets/mapa2.png";
+import { Fade } from "react-awesome-reveal";
 
 export default function National(){
     return (
-    <section className="py-3 bg-linear-to-r from-slate-950 via-slate-900 to-slate-800">
-        <Container>
-            <div className="flex w-full justify-center items-center gap-16 lg:grid-cols-2 ">
-                <div>
-                    <span className="text-sm font-semibold uppercase tracking-[0.35em] text-sky-500">
-                    Presencia en Colombia
-                    </span>
-                    <h2 className="mt-4 text-5xl font-bold text-white">Disponible a nivel nacional</h2>
-                    <div className="mt-6 h-1 w-20 rounded-full bg-sky-400" />
-                </div>
-                <div className="flex justify-center">
-                    <img src={colombiaMap} alt="Cobertura nacional" className="max-h-125 object-contain"/>
-                </div>
-            </div>
-        </Container>
-</section>
-)}
+        <Fade>
+            <section className="bg-linear-to-r from-slate-950 via-slate-900 to-slate-800 py-16 lg:py-6">
+                <Container>
+                    <div className="flex flex-col items-center gap-10 lg:flex-row lg:justify-center lg:gap-16">
+                        <div className="text-center lg:text-left">
+                            <Fade direction="left" cascade>
+                                <span className="text-sm font-semibold uppercase tracking-[0.35em] text-sky-500">
+                                    Presencia en Colombia
+                                </span>
+                                <h2 className="mt-4 text-4xl font-bold leading-tight text-white md:text-5xl">Disponible a nivel nacional</h2>
+                                <div className="mx-auto mt-6 h-1 w-20 rounded-full bg-sky-400 lg:mx-0" />
+                            </Fade>
+                        </div>
+                        <div className="flex justify-center">
+                            <Fade>
+                                <img src={colombiaMap} alt="Cobertura nacional" className="w-65 md:w-85 lg:w-105 xl:w-120" />
+                            </Fade>
+                        </div>
+                    </div>
+                </Container>
+            </section>
+        </Fade>
+        )}
 
