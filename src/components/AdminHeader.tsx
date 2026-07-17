45d1b25366d6e28d3b72e2f4b6e383130308e5a6
Paola Gutierrez <paola@justsomecoding.com>
2026-07-17 17:04:37 -0600

    mejoras

--- src/components/AdminHeader.tsx
+++ src/components/AdminHeader.tsx
@@ -6,7 +6,7 @@ export default function AdminHeader(){
     return (
       <header className="sticky top-0 z-50 border-b border-slate-200 bg-linear-to-r from-slate-950 via-slate-900 to-slate-800">
         <div className="mx-auto flex h-20 max-w-[1440px] items-center justify-between px-6">
-          <Link  to="/" target="_blanc" className="flex items-center gap-3">
+          <Link  to="/" target="_blank" className="flex items-center gap-3">
               <img src={logoBlanco} alt="P&T Logo" className="h-15 w-15 object-contain"/>
                 <div>
                   <p className="text-lg font-bold text-white">Admin</p>
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -7,15 +7,15 @@ import {
 export function Socials(){
     return (
         <div className="flex gap-4 mt-6">
-            <a href="https://www.linkedin.com/" target="_blanc">
+            <a href="https://www.linkedin.com/" rel="noopener noreferrer" target="_blank" aria-label="Visitar LinkedIn de Proofs and Trials">
                 <FaLinkedin className="text-2xl hover:text-sky-500 transition" />
             </a>
 
-            <a href="https://www.instagram.com/" target="_blanc">
+            <a href="https://www.instagram.com/" rel="noopener noreferrer" target="_blank" aria-label="Visitar Instagram de Proofs and Trials">
                 <FaInstagram  className="text-2xl hover:text-purple-400 transition" />
             </a>
 
-            <a href="https://wa.me/5731332381467}" target="_blanc">
+            <a href="https://wa.me/5731332381467" rel="noopener noreferrer" target="_blank" aria-label="Contactar a Proofs and Trials por WhatsApp">
                 <FaWhatsapp className="text-2xl hover:text-green-500 transition" />
             </a>
         </div>
